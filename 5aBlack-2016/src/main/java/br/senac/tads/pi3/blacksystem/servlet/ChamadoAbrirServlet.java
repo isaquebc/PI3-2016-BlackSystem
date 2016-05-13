b2a722cc3f9fc8119e083d633e7b477c88dd363e
Isaquebc <>
2016-05-12 21:44:09 -0300

    Trabalhando tipo Date.

--- 5aBlack-2016/src/main/java/br/senac/tads/pi3/blacksystem/servlet/ChamadoAbrirServlet.java
+++ 5aBlack-2016/src/main/java/br/senac/tads/pi3/blacksystem/servlet/ChamadoAbrirServlet.java
@@ -73,6 +73,8 @@ public class ChamadoAbrirServlet extends HttpServlet {
         
         CadastroDAO cadChamado = new CadastroDAO();
         
+        
+        
         Calendar ca = GregorianCalendar.getInstance();  
         Date data = ca.getTime();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
