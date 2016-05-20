877c880d95ac1bcc1ff26fadbfb2fb468d9c5689
Isaquebc <>
2016-05-19 21:13:32 -0300

    Atualização da tela de Gerar Pedido

--- 5aBlack-2016/src/main/webapp/_js/listaPedido.js
+++ 5aBlack-2016/src/main/webapp/_js/listaPedido.js
@@ -6,40 +6,42 @@
 
 
 window.addEventListener("load", iniciar);
-var peca, servico, tecido, quantidade, cor, dtRetirada, cont=-1;
+var peca, servico, tecido, quantidade, cor, dtRetirada, cont = -1;
 
 function iniciar() {
 
     document.getElementById('inserir').onclick = function () {
-       insere();
+        insere();
     };
 }
 
 function insere() {
-    cont++;
-    var peca = document.getElementById("tipoPeca").value; 
+
+    var peca = document.getElementById("tipoPeca").value;
     var servico = document.getElementById("tipoServico").value;
     var tecido = document.getElementById("tipoTecido").value;
     var quantidade = document.getElementById("qtdPeca").value;
     var cor = document.getElementById("cor").value;
     var dtRetirada = document.getElementById("dtRetirada").value;
-        
-     if(peca === ""|| servico === ""|| tecido === "" || quantidade === "" || cor === "" || dtRetirada === ""){
-     }
-     else{
-    $('#tabela').append('<tr id="'+cont+'"></tr>');
-    $('#'+cont+'').append('<td><input type="text" class="esconder" name="peca'+cont+'" value="'+servico+'" readonly/></td>');
-    $('#'+cont+'').append('<td><input type="text" class="esconder" name="peca'+cont+'" value="'+peca+'" readonly/></td>');
-    $('#'+cont+'').append('<td><input type="text" class="esconder" name="peca'+cont+'" value="'+tecido+'" readonly/></td>');
-    $('#'+cont+'').append('<td><input type="text" class="esconder" name="peca'+cont+'" value="'+quantidade+'" readonly/></td>');
-    $('#'+cont+'').append('<td><input type="text" class="esconder" name="peca'+cont+'" value="'+cor+'" readonly/></td>');
-    $('#'+cont+'').append('<td><input type="text" class="esconder" name="peca'+cont+'" value="'+dtRetirada+'" readonly/></td>');
 
-    document.getElementById("tipoPeca").value = ""; 
-    document.getElementById("tipoServico").value = "";
-    document.getElementById("tipoTecido").value = "";
-    document.getElementById("qtdPeca").value = "";
-    document.getElementById("cor").value = "";
-    document.getElementById("dtRetirada").value = "";
-     }
+    if (peca === "" || servico === "" || tecido === "" || quantidade === "" || cor === "" || dtRetirada === "") {
+    }
+    else {
+        cont++;
+        $('#tabela').append('<tr id="' + cont + '"></tr>');
+        $('#' + cont + '').append('<td><input type="text" class="esconder" name="peca' + cont + '" value="' + servico + '" readonly/></td>');
+        $('#' + cont + '').append('<td><input type="text" class="esconder" name="peca' + cont + '" value="' + peca + '" readonly/></td>');
+        $('#' + cont + '').append('<td><input type="text" class="esconder" name="peca' + cont + '" value="' + tecido + '" readonly/></td>');
+        $('#' + cont + '').append('<td><input type="text" class="esconder" name="peca' + cont + '" value="' + quantidade + '" readonly/></td>');
+        $('#' + cont + '').append('<td><input type="text" class="esconder" name="peca' + cont + '" value="' + cor + '" readonly/></td>');
+        $('#' + cont + '').append('<td><input type="text" class="esconder" name="peca' + cont + '" value="' + dtRetirada + '" readonly/></td>');
+
+        document.getElementById("tipoPeca").value = "";
+        document.getElementById("tipoServico").value = "";
+        document.getElementById("tipoTecido").value = "";
+        document.getElementById("qtdPeca").value = "";
+        document.getElementById("cor").value = "";
+        document.getElementById("dtRetirada").value = "";
+        document.getElementById("cont").value = cont; 
+    }
 }
