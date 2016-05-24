3d5d58e1c93eee37a18858e72615b189964c65ae
Rafael <Rafael@DESKTOP-UI3CJ4S>
2016-05-24 00:28:42 -0300

    RESOLVIDO PROBLEMA INTEGRAÇÃO CADASTRO CLIENTE

--- 5aBlack-2016/src/main/java/br/senac/tads/pi3/blacksystem/ablack/FuncinarioCadastroDAO.java
+++ 5aBlack-2016/src/main/java/br/senac/tads/pi3/blacksystem/ablack/FuncinarioCadastroDAO.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.senac.tads.pi3.blacksystem.ablack;
 
 import br.senac.tads.pi3.blacksystem.entity.Cliente;
-import br.senac.tads.pi3.blacksystem.entity.Filial;
 import br.senac.tads.pi3.blacksystem.entity.Funcionario;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,43 +15,43 @@ import java.util.logging.Logger;
  */
 public class FuncinarioCadastroDAO extends Conexao {
 
-     
-    public void cadastrarFuncionario(Funcionario func,Filial filial)
+String QUERY_INSERT_FUNCIORARIO = "insert into FUNCIONARIO(CPF_FUNC, NOME_FUNC,SOBRENOME_FUNC, SEXO_FUNC, NASC_FUNC,TEL_FUNC,"
++ " CEL_FUNC, EMAIL_FUNC, STATUS_FUNC, SENHA_FUNC, CARGO_FUNC)"+"values(?,?,?,?,?,?,?,?,?,?,?)";
+
+    public void cadastrarFuncionario(Funcionario func)
             throws ClassNotFoundException {
 
-String QUERY_INSERT_FUNCIORARIO = "INSERT INTO FUNCIONARIO( CPF_FUNC, NOME_FUNC, SOBRENOME_FUNC,SEXO_FUNC,NASC_FUNC,"
- +"TEL_FUNC ,CEL_FUNC,EMAIL_FUNC, STATUS_FUNC,SENHA_FUNC,CARGO_FUNC,SALARIO_FUNC,ID_FILIAL)"+ "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
-
+        String testeSenha="";
         Connection conn = null;
         PreparedStatement stm = null;
-                
-            try {
-                conn = getConexao();
-                String sql = QUERY_INSERT_FUNCIORARIO;
-                stm = conn.prepareStatement(sql);
-                stm.setString(1, func.getCpf());
-                stm.setString(2, func.getNome());
-                stm.setString(3, func.getSobrenome());
-                stm.setString(4,"M");
-                stm.setDate(5,Date.valueOf(func.getDataNascimento()));
-                stm.setString(6, func.getTelefone());
-                stm.setString(7, func.getCelular());
-                stm.setString(8, func.getEmail());
-                stm.setString(9, "ATIVO");
-                stm.setString(10, func.getSenha());
-                stm.setString(11, func.getCargo());
-                stm.setDouble(12, func.getSalario());
-                stm.setInt(13,(1));
-                
-                stm.executeUpdate();
-                stm.close();
-                conn.close();
-            } catch (SQLException e) {
-                System.out.println("Erro de conexão: Cadastro do cliente");
-            } catch (NullPointerException e) {
-                System.out.println("Dao não inicializado: Cadastro do cliente");
-            }
+        
+        try {
+            testeSenha= func.getSenha();
+            conn = getConexao();
+            String sql= QUERY_INSERT_FUNCIORARIO;
+            stm = conn.prepareStatement(sql);
+            stm.setString(1, func.getCpf());
+            stm.setString(2, func.getNome());
+            stm.setString(3, func.getSobrenome());
+            stm.setString(4, func.getSexo());
+            stm.setDate(5, new java.sql.Date(1009 - 03 - 03));
+            stm.setString(6, func.getTelefone());
+            stm.setString(7, func.getCelular());
+            stm.setString(8, func.getEmail());
+            stm.setString(9, "ATIVO");
+            stm.setString(10, func.getSenha());
+            stm.setString(11, func.getCargo());
 
+            
+            stm.executeUpdate();
+            stm.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Erro de conexão");
+        } catch (NullPointerException e) {
+            System.out.println("Dao não inicializado");
         }
+
+    }
 
 }
